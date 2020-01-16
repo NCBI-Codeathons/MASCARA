44d98cad959d65aec78200d707403eff1d5343a3
gomeznick86 <57917990+gomeznick86@users.noreply.github.com>
2020-01-16 14:22:28 -0500

    update needed packages
    chromVARmotifs, motifmatchr, JASPAR3016

--- docker/r_packages.R
+++ docker/r_packages.R
@@ -2,13 +2,15 @@
 ## CRAN PACKAGES
 ################################################################################
 
+install.packages("ggpubr")
+
 ################################################################################
 ## BIOCONDUCTOR PACKAGES
 ################################################################################
 BiocManager::install(c("GenomicRanges", "Gviz", "rtracklayer", 'BiocGenerics',
                        'DelayedArray', 'DelayedMatrixStats',
                        'limma', 'S4Vectors', 'SingleCellExperiment',
-                       'SummarizedExperiment', 'batchelor', 'chromVAR'))
+                       'SummarizedExperiment', 'batchelor', 'chromVAR','JASPAR2016','motifmatchr'))
 
 ################################################################################
 ## GITHUB PACKAGES
@@ -24,3 +26,6 @@ devtools::install_github("cole-trapnell-lab/cicero-release", ref="monocle3")
 ## ACTIONet
 devtools::install_github("shmohammadi86/NetLibR")
 devtools::install_github("shmohammadi86/ACTIONet")
+
+## ChromVar optional
+devtools::install_github("GreenleafLab/chromVARmotifs")
