921bc2d65327157ab7a31a532c5c6259d057b3bb
gomeznick86 <57917990+gomeznick86@users.noreply.github.com>
2020-01-16 15:46:42 -0500

    Create r_packages.R

---
+++ docker/r_packages.R
@@ -0,0 +1,32 @@
+################################################################################
+## CRAN PACKAGES
+################################################################################
+
+install.packages("ggpubr")
+
+################################################################################
+## BIOCONDUCTOR PACKAGES
+################################################################################
+BiocManager::install(c("GenomicRanges", "Gviz", "rtracklayer", 'BiocGenerics',
+                       'DelayedArray', 'DelayedMatrixStats',
+                       'limma', 'S4Vectors', 'SingleCellExperiment',
+                       'SummarizedExperiment', 'batchelor', 'chromVAR',
+                       'JASPAR2016','motifmatchr'))
+
+################################################################################
+## GITHUB PACKAGES
+################################################################################
+
+## Monocle 3
+devtools::install_github('cole-trapnell-lab/leidenbase')
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+## Cicero
+devtools::install_github("cole-trapnell-lab/cicero-release", ref="monocle3")
+
+## ACTIONet
+devtools::install_github("shmohammadi86/NetLibR")
+devtools::install_github("shmohammadi86/ACTIONet")
+
+## ChromVar optional
+devtools::install_github("GreenleafLab/chromVARmotifs")
